8310ffb0c00d7907f4b6d77c6b142ac776c03360
julian <ornatus30@gmail.com>
2021-08-21 00:17:27 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Newcomp from "./components/newcomp/newcomp";
+
+const App = () => {
+  return (
+    <div>
+      <Newcomp />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/newcomp/newcomp.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./newCompStyle.css";
+
+const Newcomp = () => {
+  return (
+    <div className="container">
+      <div className="divone">
+        <div className="divonea one">
+          <div className="cardContainer">df</div>
+          <div className="cardContainer">df</div>
+          <div className="cardContainer">df</div>
+        </div>
+        <div className="divoneb one">ff</div>
+        <div className="divonec one">ff</div>
+      </div>
+      <div className="divtwo">
+        <div className="divtwoa two">ff</div>
+        <div className="divtwob two">ff</div>
+        <div className="divtwoc two">ff</div>
+      </div>
+      <div className="divthree">
+        <div className="divhreea three">ff</div>
+        <div className="divthreeb three">ff</div>
+        <div className="divthreec three">ff</div>
+      </div>
+    </div>
+  );
+};
+
+export default Newcomp;
